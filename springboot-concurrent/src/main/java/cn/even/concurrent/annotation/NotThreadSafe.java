ebbacad9210a461608c4f37e65b9fa5876719d11
even-v <eleven896491@163.com>
2020-04-14 20:05:37 +0800

    init annotation

---
+++ springboot-concurrent/src/main/java/cn/even/concurrent/annotation/NotThreadSafe.java
@@ -0,0 +1,15 @@
+package cn.even.concurrent.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 线程不安全
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+    String value() default "";
+}
