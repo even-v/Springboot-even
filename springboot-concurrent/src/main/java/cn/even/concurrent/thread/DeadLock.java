0e29cd838af243b4c801d3c62ad2eaf1f4a9b663
even-v <eleven896491@163.com>
2020-04-17 21:47:03 +0800

    Thread & Lock

---
+++ springboot-concurrent/src/main/java/cn/even/concurrent/thread/DeadLock.java
@@ -0,0 +1,64 @@
+package cn.even.concurrent.thread;
+
+import cn.even.concurrent.annotation.NotRecommend;
+import cn.even.concurrent.annotation.NotThreadSafe;
+
+/**
+ * FileName: DeadLock
+ * Author:   jiangjingwei@yysoft.org.cn
+ * Date:     2020/4/17 21:21
+ * Description: 死锁
+ */
+@NotThreadSafe
+@NotRecommend
+public class DeadLock {
+    public static void main(String[] args) {
+        StringBuffer s1 = new StringBuffer();
+        StringBuffer s2 = new StringBuffer();
+
+
+        new Thread() {
+            @Override
+            public void run() {
+                synchronized (s1) {
+                    s1.append("a1");
+
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    synchronized (s2) {
+                        s2.append("b2");
+
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+            }
+        }.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (s2) {
+                    s1.append("c1");
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    synchronized (s1) {
+                        s2.append("d2");
+
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+            }
+        }).start();
+    }
+}
