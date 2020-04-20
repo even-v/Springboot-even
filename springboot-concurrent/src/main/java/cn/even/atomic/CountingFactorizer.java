b0ddee96e4d84166bc3e65a2f758e77f7e841d44
even-v <eleven896491@163.com>
2020-04-20 22:10:08 +0800

    Thread atomic

---
+++ springboot-concurrent/src/main/java/cn/even/atomic/CountingFactorizer.java
@@ -0,0 +1,49 @@
+package cn.even.atomic;
+
+import cn.even.concurrent.annotation.ThreadSafe;
+import lombok.Getter;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * FileName: CountingFactorizer
+ * Author:   jiangjingwei@yysoft.org.cn
+ * Date:     2020/4/20 21:15
+ * Description:对于多线程来说，操作count是原子性的
+ */
+@ThreadSafe
+public class CountingFactorizer implements Servlet {
+
+    @Getter
+    private final AtomicLong count = new AtomicLong(0);
+
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        count.incrementAndGet();
+        req.getRequestDispatcher("").forward(req, res);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ springboot-concurrent/src/main/java/cn/even/atomic/UnsafeCachingFactorizer.java
@@ -0,0 +1,68 @@
+package cn.even.atomic;
+
+import cn.even.concurrent.annotation.NotThreadSafe;
+import lombok.Getter;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * FileName: UnsafeCountingFactorizer
+ * Author:   jiangjingwei@yysoft.org.cn
+ * Date:     2020/4/20 21:01
+ * Description: 没有足够原子性保证情况下对其最近计算结果进行缓存
+ */
+@NotThreadSafe
+public class UnsafeCachingFactorizer implements Servlet {
+
+    private final AtomicReference<BigInteger> lastNumber = new AtomicReference<>();
+    private final AtomicReference<BigInteger[]> lastFactors = new AtomicReference<>();
+
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        BigInteger i = extractFormRequest(req);
+        if (i.equals(lastNumber.get())) {
+            encodeIntoResponse(res, lastFactors.get());
+        } else {
+            BigInteger[] factors = factor(i);
+            lastNumber.set(i);
+            lastFactors.set(factors);
+            encodeIntoResponse(res, factors);
+        }
+    }
+
+    private BigInteger[] factor(BigInteger i) {
+        return null;
+    }
+
+    private void encodeIntoResponse(ServletResponse res, BigInteger[] bigIntegers) {
+    }
+
+    private BigInteger extractFormRequest(ServletRequest req) {
+        return null;
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
